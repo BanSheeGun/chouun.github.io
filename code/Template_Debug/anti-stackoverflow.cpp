fd203c1621fc384f461f2d03e29c0e5e02dde5f5
ChouUn <chouunsoft@gmail.com>
2015-09-11 16:35:56 +0800

    Site updated: 2015-09-11 16:35:56

--- code/Template_Debug/anti-stackoverflow.cpp
+++ code/Template_Debug/anti-stackoverflow.cpp
@@ -8,13 +8,17 @@ void Main() {
 
 // Anti Stack Overflow
 int main() {
+#ifdef _ANTI_STACKOVERFLOW
   int _size = 16 << 20; // MB : << 20 ; KB : << 10 ;
   char* _data = (char*)malloc(_size) + _size;
+  __asm__ __volatile__(
 #if __x86_64__ || __ppc64__ || _WIN64
-  __asm__ __volatile__("movq %0,%%rsp\n pushq $_exit\n"::"r"(_data));
+      "movq  %0, %%rsp\n pushq $_exit\n"
 #else // 64-bit
-  __asm__ __volatile__("movl %0,%%esp\n pushq $_exit\n"::"r"(_data));
+      "movl  %0, %%esp\n pushl $_exit\n"
 #endif // 32-bit
+      :: "r"(_data));
+#endif // _ANTI_STACKOVERFLOW
   Main();
   exit(0);
 }
--- code/Template_Release/anti-stackoverflow.cpp
+++ code/Template_Release/anti-stackoverflow.cpp
@@ -1,6 +1,5 @@
 //@ Including Header
 #include <csl_std.h>
-#define _ANTI_STACKOVERFLOW
 
 //@ Main Function
 void Main() {
@@ -9,17 +8,13 @@ void Main() {
 
 // Anti Stack Overflow
 int main() {
-#ifdef _ANTI_STACKOVERFLOW
   int _size = 16 << 20; // MB : << 20 ; KB : << 10 ;
   char* _data = (char*)malloc(_size) + _size;
-  __asm__ __volatile__(
 #if __x86_64__ || __ppc64__ || _WIN64
-      "movq  %0, %%rsp\n pushq $_exit\n"
+  __asm__ __volatile__("movq %0,%%rsp\n pushq $_exit\n"::"r"(_data));
 #else // 64-bit
-      "movl  %0, %%esp\n pushl $_exit\n"
+  __asm__ __volatile__("movl %0,%%esp\n pushq $_exit\n"::"r"(_data));
 #endif // 32-bit
-      :: "r"(_data));
-#endif // _ANTI_STACKOVERFLOW
   Main();
   exit(0);
 }
