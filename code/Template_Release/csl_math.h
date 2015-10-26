94d04b74b35bbf45701d8113ecf736e7cc8ff58b
ChouUn <chouunsoft@gmail.com>
2015-10-26 15:44:38 +0800

    Site updated: 2015-10-26 15:44:37

--- code/Template_Release/csl_math.h
+++ code/Template_Release/csl_math.h
@@ -6,24 +6,34 @@
 namespace csl {
   template <typename _Tp>
   inline _Tp gcd(_Tp a, _Tp b)
-  { while (b) { _Tp t=a%b; a=b; b=t; } return a; }
+  { while (b) { _Tp t = a%b; a=b; b=t; } return a; }
   template <typename _Tp>
   _Tp gcd(_Tp a, _Tp b, _Tp& x, _Tp& y)
   { if (b) { _Tp r=gcd(b,a%b,y,x); return y=y-a/b*x,r; } return x=1,y=0,a; }
   template <typename _Tp>
   inline _Tp lcm(_Tp a, _Tp b)
   { return a / gcd(a,b) * b; }
-  template <typename _Tp>
-  inline _Tp mul(_Tp a, _Tp b, const _Tp m)
-  { _Tp c=0; for (a%=m; b; b>>=1,a=(a<<1)%m) if (b&1) c=(c+a)%m; return c; }
+  template <typename _Val, typename _Key, typename _Mod,
+            typename _Op1, typename _Op2>
+  inline _Val dnc(_Val c, _Val n, _Key k, const _Mod m,
+                  const _Op1& op1, const _Op2& op2) {
+    for (n=op2(n,m);k;n=op2(op1(n,n),m),k>>=1) if (k&1) c=op2(op1(c,n),m);
+    return c;
+  }
+  template <typename _Val, typename _Key, typename _Op>
+  inline _Val dnc(_Val c, _Val n, _Key k, const _Op& op)
+  { for (;k;n=op(n,n),k>>=1) if (k&1) c=op(c,n); return c; }
+  template <typename _Val>
+  inline _Val mul(_Val a, _Val b, const _Val m)
+  { return dnc(_Val(),a,b,m,std::plus<_Val>(),std::modulus<_Val>()); }
   template <typename _Val, typename _Key>
   inline _Val pow(_Val c, _Val n, const _Key k)
-  { for (; k; n=n*n,k>>=1) if (k&1) c=c*n; return c; }
+  { return dnc(c,n,k,std::multiplies<_Val>()); }
   template <typename _Val, typename _Key>
   inline _Val pow(_Val c, _Val n, _Key k, const _Val m)
-  { for (n%=m; k; n=n*n%m,k>>=1) if (k&1) c=c*n%m; return c; }
-  template <typename _Val, typename _Key>
-  inline _Val inv(_Val x, const _Key m)
-  { return csl::pow(_Val(1),x,m-2,m); }
+  { return dnc(c,n,k,m,std::multiplies<_Val>(),std::modulus<_Val>()); }
+  template <typename _Val>
+  inline _Val inv(_Val x, const _Val m)
+  { return pow(_Val(1),x,m-2,m); }
 } // namespace csl
 #endif /* CSL_MATH_H_ */
